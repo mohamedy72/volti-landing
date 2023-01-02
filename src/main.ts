4730aa54eb46aee57e54bbff4001c67c55653b27
mohamedy72 <mohamedwebdev94@gmail.com>
2023-01-02 17:33:45 +0200

    feat: Add Back To Top functionality

--- src/main.ts
+++ src/main.ts
@@ -29,13 +29,15 @@ navToggler?.addEventListener("click", toggleNavbar);
  */
 
 const header = document.querySelector("[data-header]");
+const backTopBtn = document.querySelector("[data-go-top-btn]");
 window.addEventListener("scroll", () => {
   if (window.scrollY >= 100) {
     header?.classList.add("active");
+    backTopBtn?.classList.add("active");
   } else {
     header?.classList.remove("active");
+    backTopBtn?.classList.remove("active");
   }
 });
-
 
 export {};
